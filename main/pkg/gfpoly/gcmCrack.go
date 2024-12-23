daafd7af0a76ba1f562c96c0aeb2b6a4abce0dc3
Justus Siegert <jubesie@googlemail.com>
2024-12-23 17:01:26 +0100

    remove Multithreading in gcmCrack

--- main/pkg/gfpoly/gcmCrack.go
+++ main/pkg/gfpoly/gcmCrack.go
@@ -4,7 +4,6 @@ import (
 	"Abgabe/main/pkg/actions"
 	"Abgabe/main/pkg/utils"
 	"math/big"
-	"sync"
 )
 
 type message struct {
@@ -36,6 +35,8 @@ type mes struct {
 }
 
 func (args *GcmCrack) Execute() {
+	//nonce := utils.NewLongFromBase64(args.Nonce).Int
+
 	messages := make([]mes, 0, 3)
 	for _, m := range []message{args.M1, args.M2, args.M3} {
 		me := new(mes)
@@ -47,8 +48,9 @@ func (args *GcmCrack) Execute() {
 		messages = append(messages, *me)
 	}
 	poly := new(Poly).Add(&messages[0].Poly, &messages[1].Poly)
+	//H candiandes calculation
 	candidates := poly.FindRoots()
-
+	//calculation of GHASH
 	for _, candidate := range candidates {
 		ghash := actions.GHASHBigEndian(candidate, messages[0].Ciphertext, messages[0].L, messages[0].AssociatedData)
 		mask := ghash.Xor(&ghash, &messages[0].Tag)
@@ -57,53 +59,42 @@ func (args *GcmCrack) Execute() {
 		if tagM3.Cmp(&messages[2].Tag) == 0 {
 			forgeryCipherText := utils.GetContent(args.Forgery.Ciphertext)
 			forgeryAd := utils.GetContent(args.Forgery.AssociatedData)
+
 			_, forgeryL := actions.CalculateL(args.Forgery.Ciphertext, args.Forgery.AssociatedData)
 			resultGhash := actions.GHASHBigEndian(candidate, forgeryCipherText, forgeryL, forgeryAd)
+
 			forgeryTag := *new(big.Int).Xor(&resultGhash, mask)
+
 			args.Tag = utils.NewLongFromBigInt(forgeryTag).GcmToggle().GetBase64(16)
 			args.H = utils.NewLongFromBigInt(candidate).GcmToggle().GetBase64(16)
 			args.Mask = utils.NewLongFromBigInt(*mask).GcmToggle().GetBase64(16)
 			return
 		}
 	}
+
 	return
 }
+
 func (p *Poly) FindRoots() []big.Int {
 	factors := p.sff()
-	candidates := make(chan big.Int, len(factors))
+	candidates := []big.Int{}
 
-	var wg sync.WaitGroup
 	for _, factor := range factors {
-		wg.Add(1)
-		go func(factor Factor) {
-			defer wg.Done()
-			if factor.Factor.Degree() > 1 {
-				ddfFactors := factor.Factor.Ddf()
-				for _, dFactor := range ddfFactors {
-					if dFactor.Factor.Degree() == 1 {
-						candidates <- dFactor.Factor[0]
-					} else if dFactor.Exponent == 1 {
-						edfFactors := new(Polys).Edf(&dFactor.Factor, dFactor.Exponent)
-						for _, eFactor := range edfFactors {
-							if eFactor.Degree() == 1 {
-								candidates <- eFactor[0]
-							}
+		if factor.Factor.Degree() > 1 {
+			ddfFactors := factor.Factor.Ddf()
+			for _, dFactor := range ddfFactors {
+				if dFactor.Factor.Degree() == 1 {
+					candidates = append(candidates, dFactor.Factor[0])
+				} else if dFactor.Exponent == 1 {
+					edfFactors := new(Polys).Edf(&dFactor.Factor, dFactor.Exponent)
+					for _, eFactor := range edfFactors {
+						if eFactor.Degree() == 1 {
+							candidates = append(candidates, eFactor[0])
 						}
 					}
 				}
 			}
-		}(factor)
+		}
 	}
-
-	go func() {
-		wg.Wait()
-		close(candidates)
-	}()
-
-	var result []big.Int
-	for candidate := range candidates {
-		result = append(result, candidate)
-	}
-
-	return result
+	return candidates
 }
